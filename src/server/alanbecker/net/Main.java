be65bd90b0ae3fc55f596460694eb0eb4cd97edb
MasterGabeMOD <MasterGabeMod@yahoo.com>
2024-01-14 02:46:56 -0800

    Fix performance issues
    Thank goodness for Spark

--- src/server/alanbecker/net/Main.java
+++ src/server/alanbecker/net/Main.java
@@ -27,8 +27,8 @@ public class Main extends JavaPlugin implements Listener {
         getLogger().info("AFKPlugin has been enabled!");
         getServer().getPluginManager().registerEvents(this, this);
 
-  
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, this::checkAFKPlayers, 20L, 20L);
+
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, this::checkAFKPlayersAsync, 20L, 20L);
     }
 
     @Override
@@ -40,7 +40,6 @@ public class Main extends JavaPlugin implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
 
- 
         if (player.hasPermission(IGNORE_PERMISSION)) {
             return;
         }
@@ -65,21 +64,23 @@ public class Main extends JavaPlugin implements Listener {
     public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
 
-   
         setAFKStatus(player, false);
+    }
+
+    private void checkAFKPlayersAsync() {
+        Bukkit.getScheduler().runTask(this, this::checkAFKPlayers);
     }
 
     private void checkAFKPlayers() {
         for (Player player : Bukkit.getOnlinePlayers()) {
             if (!player.hasPermission(IGNORE_PERMISSION) && isAFK(player) && !hasRecentlyJoined(player)) {
-       
                 setAFKStatus(player, true);
             }
         }
     }
 
     private boolean hasRecentlyJoined(Player player) {
-    	return System.currentTimeMillis() - joinTimeMap.getOrDefault(player, 0L) < 120000;
+        return System.currentTimeMillis() - joinTimeMap.getOrDefault(player, 0L) < 120000;
     }
 
     private boolean isAFK(Player player) {
@@ -93,9 +94,8 @@ public class Main extends JavaPlugin implements Listener {
                     ChatColor.RED + "You're AFK!",
                     ChatColor.YELLOW + "Move to get out of spectator!",
                     10, 100, 20
-            		);
-        }
-        else {
+            );
+        } else {
             player.setGameMode(GameMode.SURVIVAL);
             player.sendTitle("", "", 0, 0, 0);
         }
